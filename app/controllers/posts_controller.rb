0a359e5e99640b39298c462fa08fb0311ab04c4b
Moncrieff <dmitry.veremchuk@gmail.com>
2015-12-25 20:40:43 +0300

    Create new post feature complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,26 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Новая запись успешно опубликована."
+      redirect_to @post
+    else
+      # insert the flash for unsuccessful post publication
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:header, :reading_time, :excerpt, :body)
+  end
 end
